59046c6002869d08db3ce9427ecd2026c4c2bc9b
Drew Bredvick <dbredvick@gmail.com>
2023-01-14 11:09:31 -0600

    redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://codepen.io/chriscoyier/pen/AwVNjg',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
